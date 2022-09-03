eeb7c2b677915b190130799dded09684eb0855d9
Tayyab Ahmed <mmtayyab530@gmail.com>
2022-09-03 15:12:48 +0500

    action toolbar completion

--- lib/widgets/actions_toolbar.dart
+++ lib/widgets/actions_toolbar.dart
@@ -23,18 +23,35 @@ class ActionsToolbar extends StatelessWidget {
 // The size of the plus icon under the profile image in follow action
   static const double kPlusIconSize = 20.0;
 
+  LinearGradient get musicGradient => LinearGradient(colors: [
+        Colors.grey[700]!,
+        Colors.grey[900]!,
+        Colors.grey[900]!,
+        Colors.grey[700]!
+      ], stops: const [
+        0.0,
+        0.4,
+        0.6,
+        1.0
+      ], begin: Alignment.bottomLeft, end: Alignment.topRight);
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(bottom: 30.0),
       width: 100.0,
-      color: Colors.red[300],
+      color: Colors.transparent,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          _getFollowAction(pictureUrl: "https://media-exp1.licdn.com/dms/image/C4D03AQGciJDIzpsfnw/profile-displayphoto-shrink_200_200/0/1661865090860?e=1667433600&v=beta&t=KOBMepGaAccKMS-s4GqXsSSeSSz5z8-gORlDvJ7DlzA"),
+          _getFollowAction(
+              pictureUrl:
+                  "https://media-exp1.licdn.com/dms/image/C4D03AQGciJDIzpsfnw/profile-displayphoto-shrink_200_200/0/1661865090860?e=1667433600&v=beta&t=KOBMepGaAccKMS-s4GqXsSSeSSz5z8-gORlDvJ7DlzA"),
           _getSocialAction(icon: Icons.favorite_outlined, title: '3.2m'),
           _getSocialAction(icon: Icons.comment, title: '16.4k'),
           _getSocialAction(icon: Icons.reply, title: 'Share'),
+          _getMusicPlayerAction(pictureUrl:
+          "https://media-exp1.licdn.com/dms/image/C4D03AQGciJDIzpsfnw/profile-displayphoto-shrink_200_200/0/1661865090860?e=1667433600&v=beta&t=KOBMepGaAccKMS-s4GqXsSSeSSz5z8-gORlDvJ7DlzA"),
         ],
       ),
     );
@@ -61,16 +78,15 @@ class ActionsToolbar extends StatelessWidget {
         ]));
   }
 
-  Widget _getFollowAction({
-    required String pictureUrl}) {
+  Widget _getFollowAction({required String pictureUrl}) {
     return Container(
         margin: const EdgeInsets.symmetric(vertical: 10.0),
         width: 60.0,
         height: 60.0,
-        child: Stack( children: [
+        child: Stack(children: [
           _getProfilePicture(pictureUrl: pictureUrl),
-          _getPlusIcon()])
-    );
+          _getPlusIcon()
+        ]));
   }
 
   Widget _getPlusIcon() {
@@ -78,32 +94,70 @@ class ActionsToolbar extends StatelessWidget {
       bottom: 0,
       left: ((kActionWidgetSize / 2) - (kPlusIconSize / 2)),
       child: Container(
-          width: kPlusIconSize,  // kPlusIconSize = 20.0;
+          width: kPlusIconSize, // kPlusIconSize = 20.0;
           height: kPlusIconSize, // kPlusIconSize = 20.0;
           decoration: BoxDecoration(
               color: const Color.fromARGB(255, 255, 43, 84),
-              borderRadius: BorderRadius.circular(15.0)
-          ),
-          child: const Icon(Icons.add, color: Colors.white, size: 20.0, )),);
+              borderRadius: BorderRadius.circular(15.0)),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 20.0,
+          )),
+    );
   }
 
   Widget _getProfilePicture({required String pictureUrl}) {
     return Positioned(
         left: (kActionWidgetSize / 2) - (kProfileImageSize / 2),
         child: Container(
-          padding: const EdgeInsets.all(1.0), // Add 1.0 point padding to create border
-          height: kProfileImageSize, // kProfileImageSize = 50.0;
-          width: kProfileImageSize, // kProfileImageSize = 50.0;
+          padding: const EdgeInsets.all(1.0),
+          // Add 1.0 point padding to create border
+          height: kProfileImageSize,
+          // kProfileImageSize = 50.0;
+          width: kProfileImageSize,
+          // kProfileImageSize = 50.0;
           decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(kProfileImageSize / 2)
-          ),
+              borderRadius: BorderRadius.circular(kProfileImageSize / 2)),
           // import 'package:cached_network_image/cached_network_image.dart'; at the top to use CachedNetworkImage
-          child: CachedNetworkImage(
-            imageUrl: pictureUrl,
-            placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(pictureUrl),
+            // child: CachedNetworkImage(
+            //   imageUrl: pictureUrl,
+            //   placeholder: (context, url) => const CircularProgressIndicator(),
+            //   errorWidget: (context, url, error) => const Icon(Icons.error),
+            // ),
           ),
         ));
+  }
+
+  Widget _getMusicPlayerAction({required String pictureUrl}) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      width: kActionWidgetSize,
+      height: kActionWidgetSize,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(11.0),
+            height: kProfileImageSize,
+            width: kProfileImageSize,
+            decoration: BoxDecoration(
+                gradient: musicGradient,
+                borderRadius: BorderRadius.circular(kProfileImageSize / 2)),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(pictureUrl),
+              // child: CachedNetworkImage(
+              //   imageUrl:
+              //       pictureUrl,
+              //   placeholder: (context, url) => const CircularProgressIndicator(),
+              //   errorWidget: (context, url, error) => const Icon(Icons.error),
+              // ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
